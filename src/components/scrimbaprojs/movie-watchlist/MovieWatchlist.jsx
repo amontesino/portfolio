373398d44b78982641905648ef07f5a3bb9bc1ce
A. Montesino <120063688+amontesino@users.noreply.github.com>
2024-05-08 19:01:35 -0400

    more movie watchlist importing

--- src/components/scrimbaprojs/movie-watchlist/MovieWatchlist.jsx
+++ src/components/scrimbaprojs/movie-watchlist/MovieWatchlist.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import Movie from './components/Movie'
-import './images'
 
 export default function MovieWatchlist() {
-
+    
 
     return (
-        <h1>Movie watchlist</h1>
+        <>
+            <header>
+                <h1>My Watchlist</h1>
+                <a href="index.html">Search for movies</a>
+            </header>
+            <main id="watchlist">
+                <div id="empty-search">
+                    <h3>Your watchlist is looking a little empty...</h3>
+                    <div class="add-to-watchlist">
+                        <img src="images/add.png" />
+                        <p>Let's add some movies!</p>
+                    </div>
+                </div>
+            </main>
+            <script src="watchlist.js"></script>
+        </>
     )
 }
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import MovieWatchlist from '../components/scrimbaprojs/movie-watchlist/MovieWatchlist'
 
 export default function Portfolio() {
     return (
