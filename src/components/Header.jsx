6504ecf28ee677875097ca1e03da967e7899f1fc
A. Montesino <120063688+amontesino@users.noreply.github.com>
2024-07-03 13:45:06 -0400

    new headers & slight portfolio adjustment
    -implemented FontAwesome icons
    -implemented header name text on mobile only, hiding home icon on mobile
    -replaced personal project body text with greyed out nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,37 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
-import homeIcon from "../assets/header-icons/home.svg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faPerson, faSuitcase } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header() {
     return (
         <header className="header">
             <Link className="header-name" to="/">
-                <img src={homeIcon} />
+                {/* <img src={null} /> */}
+                <FontAwesomeIcon icon={faHome} className='mobile-hidden' />
+                <h1 className="desktop-hidden">A. Montesino</h1>
             </Link>
-            <nav class="navlink-container">
+            <hr className="rounded mobile-hidden" />
+            <nav className="navlink-container">
                 {/* Figure out how to transition between NavLinks */}
                 <NavLink
                     className="header-link"
                     to="portfolio"
                     // style={({ isActive }) => isActive ? activeStyles : null}
                 >
-                    Portfolio
+                    <FontAwesomeIcon icon={faSuitcase} />
                 </NavLink>
                 <NavLink
                     className="header-link"
                     to="about"
                 >
-                    About
+                    <FontAwesomeIcon icon={faPerson} />
                 </NavLink>
                 <NavLink
                     className="header-link"
                     to="contact"
                 >
-                    Contact
+                    <FontAwesomeIcon icon={faEnvelope} />
                 </NavLink>
             </nav>
         </header>
