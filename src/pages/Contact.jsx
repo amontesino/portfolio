832da45b4e9d7e3efc876a92447e02571d60d641
A. Montesino <120063688+amontesino@users.noreply.github.com>
2024-07-06 14:38:52 -0400

    added functioning EmailJS form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Email from '../components/Email'
 
 export default function Contact() {
     return (
         <>
             <main className="page-section">
                 <h1 className="page-head">Contact</h1>
+                <Email />
             </main>
         </>
     )
