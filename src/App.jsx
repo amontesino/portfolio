720483abe971ee0f1a85a2c430dcf8ee08a82383
A. Montesino <120063688+amontesino@users.noreply.github.com>
2024-08-04 19:43:11 -0400

    fixed home-body margin, fixed contact nav links and email send button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom"
+import { BrowserRouter, Routes, Route} from "react-router-dom"
 import './App.css'
 import Home from './pages/Home'
 import About from './pages/About'
