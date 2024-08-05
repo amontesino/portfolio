d3f545638ffdaf368522e4285c1ba376e3ddcc57
A. Montesino <120063688+amontesino@users.noreply.github.com>
2024-08-04 20:38:54 -0400

    revised contents on About page according to peer review

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,11 +8,11 @@ export default function About() {
                 <h1 className="page-head">About</h1>
                 <h2>Who am I?</h2>
                 <p>
-                    I'm <span className="strong">Armando Montesino.</span> I am a frontend developer with over a year of academic and amateur experience after leaving behind my Communications degrees. If you're looking to help me add "professional" to that list of adjectives, <NavLink className="body-link" to="../contact">let's talk!</NavLink>
+                    I'm <span className="strong">Armando Montesino.</span> I am a frontend developer with over a year of bootcamp and amateur experience after gaining my Communications Bachelor's and Master's degrees. If you're looking to hire me, <NavLink className="body-link" to="../contact">let's talk!</NavLink>
                 </p>
                 <h2>What Code + Communications Brings</h2>
                 <p>
-                    Simply put, my prior history with Communications and Journalism brings a unique level of understanding to the workplace. I am more than able to put myself in the shoes of both my clients and colleagues to understand the best course of action to continue a project, or resolve a conflict. Collaborative coding is a lot more than just pulls and merge requests. It's about making a project your team is <span className="strong">proud</span> of, and making sure the journey there was one you'd <span className='strong'>want</span> to repeat.
+                    Simply put, my prior history with Communications and Journalism brings a unique level of understanding to the workplace. I am more than able to put myself in the shoes of both my clients and colleagues to understand the best course of action to continue a project. Collaborative coding is a lot more than just pulls and merge requests. It's about making a project your team is <span className="strong">proud</span> of, and making sure the journey there was one you'd <span className='strong'>want</span> to repeat.
                 </p>
                 <h2>The Dev Part</h2>
                 <p>
@@ -22,7 +22,9 @@ export default function About() {
                     <li>HTML</li>
                     <li>CSS</li>
                     <li>JavaScript</li>
+                    <li>TypeScript</li>
                     <li>React</li>
+                    <li>Firebase</li>
                 </ul>
                 <p>as well as being an avid proponent of these concepts:</p>
                 <ul>
