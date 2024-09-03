62014519f78fdc577c3adb3d8da00e142f8e691e
A. Montesino <120063688+amontesino@users.noreply.github.com>
2024-09-03 14:02:24 -0400

    added images to public dir to display on netlify properly

--- src/utils/portfolioList.js
+++ src/utils/portfolioList.js
@@ -12,21 +12,21 @@ export const scrimProjList = [
     {
         name: 'Movie Watchlist',
         description: "The purpose of this project was to demonstrate my knowledge with APIs. This particular project used the free OMDB API.",
-        image: 'src/assets/portfolio-images/watchlist-screen.png',
+        image: '/portfolio-images/watchlist-screen.png',
         componentName: 'MovieWatchlist',
         pathName: 'https://am98-movie-watchlist.netlify.app/'
     },
     {
         name: 'Restaurant Ordering App',
         description: "This app was made to solidify proper usage of Classes and Objects in JavaScript.",
-        image: "src/assets/portfolio-images/ordering-screen.png",
+        image: "/portfolio-images/ordering-screen.png",
         componentName: 'RestaurantOrdering',
         pathName: 'https://splendorous-fudge-8ab13d.netlify.app/'
     },
     {
         name: 'Travel Journal',
         description: "This is a beginner React project made to demonstrate reusable components, as well as passing Props to said components.",
-        image: "src/assets/portfolio-images/travel-screen.png",
+        image: "/portfolio-images/travel-screen.png",
         componentName: 'TravelJournal',
         pathName: 'https://dapper-fox-8d3c62.netlify.app/'
     },
@@ -36,21 +36,21 @@ export const smallProjList = [
     {
         name: 'Password Generator',
         description: "This is an intermediate JavaScript project utilizing DOM manipulation and event listeners, rendering passwords based on settings configured by the user.",
-        image: "src/assets/slideshow-images/passgen-screen.png",
+        image: "/slideshow-images/passgen-screen.png",
         // componentName: 'PasswordGenerator',
         pathName: 'https://animated-semolina-b4ff17.netlify.app/'
     },
     {
         name: 'Color Scheme Generator',
         description: "This project exemplifies usage of queries in API calls to generate color schemes according to the user-controlled color selector.",
-        image: "src/assets/portfolio-images/colorgen-screen.png",
+        image: "/portfolio-images/colorgen-screen.png",
         // componentName: 'ColorScheme',
         pathName: 'https://am98-color-scheme.netlify.app/'
     },
     {
         name: 'Digital Business Card',
         description: "This is a beginner React project made to solidify the concept of Components.",
-        image: "src/assets/portfolio-images/businesscard-screen.png",
+        image: "/portfolio-images/businesscard-screen.png",
         // componentName: 'DigitalBusinessCard',
         pathName: 'https://am98-digital-business-card.netlify.app/'
     }
