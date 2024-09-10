09a7c61d13d912bd4d7331119dc12273cac5f3cd
A. Montesino <120063688+amontesino@users.noreply.github.com>
2024-09-09 21:49:03 -0400

    changed 404 page to handle header

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function Root() {
             <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
           </Route>
-          <Route path ="*" element={<NotFound />}></Route>
+          <Route path ="*" element={<NotFound />} />
         </Routes>
       </AnimatePresence>
     </>
